b54a950812b34497fedd9ce9f94aa8bc197dffdf
Hauke Thorenz <htho@thorenz.net>
2021-09-06 19:52:17 +0200

    refactor&clean

--- index.js
+++ index.js
@@ -10,34 +10,43 @@ const consoleLogsMessage = (page, message="", timeout=3000) => new Promise((reso
     const url = page.url();
     page.on("console", (ev) => {
         const text = ev.text();
-        // console.log(url, text);
         if(text !== message) return;
         clearTimeout(handle);
         resolve();
     });
 });
 
-async function runTestInner(relPathToHtml="tests/box-metrics/offset-left.html", expectedRecalcStyleCount=0) {
+async function runTestInner(relPathToHtml="") {
+    try {
+        const tracingPath = path.join(__dirname, relPathToHtml) + ".json";
+        const screenshotPath = path.join(__dirname, relPathToHtml) + ".png";
+        
     const browser = await puppeteer.launch({});
     const page = await browser.newPage();
-    const metricsPath = path.join(__dirname, "tests", "box-metrics", "offset-left.metrics.json");
-    fs.rmSync(metricsPath, {force: true});
-    // console.log(`Storing metrics at ${metricsPath}`);
-    await page.tracing.start({path: metricsPath});
+        
+        await page.tracing.start({path: tracingPath});
     await page.goto(`file://${__dirname}/${relPathToHtml}`);
     await consoleLogsMessage(page, "end");
     await page.tracing.stop()
     const result = await page.metrics();
+        await page.screenshot({path: screenshotPath, type: "png"});
     await browser.close();
-    if(result.RecalcStyleCount !== expectedRecalcStyleCount) console.error(`!!! ${relPathToHtml} did not have the expected amount of style recalculations. Expected: ${expectedRecalcStyleCount}. Actual: ${result.RecalcStyleCount}.`);
-    else console.log(`${relPathToHtml} had the expected amount of style recalculations. Expected&Actual: ${expectedRecalcStyleCount}.`);
-}
-async function runTest(relPathToHtml="tests/box-metrics/offset-left.html", expectedRecalcStyleCount=0) {
-    try {
-        await runTestInner(relPathToHtml, expectedRecalcStyleCount);
+    
+        return result;
     } catch (error) {
         console.error(`!!! ${relPathToHtml} Exception thrown:`, error);
     }
+    return undefined;
+}
+async function runTest({testFile = "", expected = {RecalcStyleCount: 0, LayoutCount: 0}}) {
+    const relPathToHtml = path.join("tests", testFile);
+    console.log(relPathToHtml);
+    const result = await runTestInner(relPathToHtml);
+    if(result === undefined) return;
+    if(result.RecalcStyleCount !== expected.RecalcStyleCount) console.error(`!   RecalcStyleCount: ${result.RecalcStyleCount} (expected: ${expected.RecalcStyleCount})`);
+    else console.log(`    RecalcStyleCount: ${expected.RecalcStyleCount}`);
+    if(result.LayoutCount !== expected.LayoutCount) console.error(`!   LayoutCount: ${result.LayoutCount} (expected: ${expected.LayoutCount})`);
+    else console.log(`    LayoutCount: ${expected.LayoutCount}`);
 }
 
 (async () => {
